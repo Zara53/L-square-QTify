e89a4a29f086e3b974bc309bea2fd23965a93e2d
Zara53 <r15530700@gmail.com>
2023-10-17 21:15:12 +0530

    cardCarouselTopAlbums-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
-import Card from "./components/Card/Card";
+// import Card from "./components/Card/Card";
 import { useEffect, useState } from "react";
 import { fetchTopAlbums } from "./api/api";
+import Section from "./components/Section/Section";
 
 function App() {
   const [topAlbumsData, setTopAlbumsData] = useState([]);
@@ -22,9 +23,12 @@ function App() {
     <div>
       <Navbar />
       <Hero />
-      {topAlbumsData.map((item) => {
+      {/* {topAlbumsData.map((item) => {
         return <Card data={item} type="album" key={item.title} />;
-      })}
+      })} */}
+      <div>
+        <Section data={topAlbumsData} type="album" title="Top Albums" />
+      </div>
     </div>
   );
 }
